439dfebc9dfa925b72cf825ec509cf03b5be983f
huangzy <huangzy@glodon.com>
2020-02-28 18:15:22 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,9 @@ module.exports = {
   base: "/vuepress/",
   title: "zhierblog",
   description: "这是止耳的vuepress博客",
+  // 如果网址不是部署在根目录下，favicon.ico必须指定
   head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+
   configureWebpack: {
     resolve: {
       alias: {
@@ -10,10 +12,16 @@ module.exports = {
       }
     }
   },
+  evergreen: false, //浏览器兼容性，如果你的对象只有那些 “常青树” 浏览器，你可以将其设置成 true，这将会禁止 ESNext 到 ES5 的转译以及对 IE 的 polyfills，同时会带来更快的构建速度和更小的文件体积。
   markdown: {
     anchor: { permalink: true },
-    toc: { includeLevel: [1, 2, 3] },
-    lineNumbers: true
+    toc: {
+      includeLevel: [1, 2, 3],
+      containerHeaderHtml: '<div class="toc-container-header">Contents</div>',
+      containerFooterHtml: '<div class="toc-container-footer">Footer</div>'
+    },
+    lineNumbers: true,
+    extractHeaders: ["h2", "h3", "h4"]
   },
   plugins: [
     // 你可以多次使用这个插件
---
+++ docs/module/commonjs.md
@@ -0,0 +1,3 @@
+# commonjs
+
+[js 模块化编程之彻底弄懂 CommonJS 和 AMD/CMD！](https://www.cnblogs.com/moxiaowohuwei/p/8692359.html)
