2787935f05cd9312272325c9cf6b657f23f065c8
huangzy <huangzy@glodon.com>
2020-03-31 03:06:28 +0800

    update

---
+++ docs/.vuepress/components/daily/status.vue
@@ -0,0 +1,27 @@
+<template>
+  <span :style="style_type">{{text}}</span>
+</template>
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "success"
+    }
+  },
+  computed: {
+    text() {
+      return this.type === "success" ? "（完成）" : "（未完成）";
+    },
+    style_type() {
+      var obj = {};
+      if (this.type === "success") {
+        obj["color"] = "green";
+      } else {
+        obj["color"] = "red";
+      }
+      return obj;
+    }
+  }
+};
+</script>
