50ca3b73773e49f2a219062ba4b406059aabd95d
huangzy <huangzy@glodon.com>
2020-10-22 18:25:01 +0800

    update

--- server/routes/test.js
+++ server/routes/test.js
@@ -2,10 +2,10 @@ const Router = require("koa-router");
 const path = require("path");
 const router = new Router({ prefix: "/test" });
 
-router.get("/api", async (ctx) => {
+router.get("/api", async ctx => {
   ctx.body = {
     code: 200,
-    message: "我好中意你！",
+    message: "错在陈醋当成墨，写尽半生纸上酸。"
   };
 });
 
